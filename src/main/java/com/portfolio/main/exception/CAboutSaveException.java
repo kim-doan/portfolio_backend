3d26be41af03b93a7d396a159858ebd08b135fbb
kimdoan <doan0105@e3view.com>
2021-11-18 18:45:09 +0900

    no message

--- src/main/java/com/portfolio/main/exception/CAboutSaveException.java
+++ src/main/java/com/portfolio/main/exception/CAboutSaveException.java
@@ -1,6 +1,7 @@
 package com.portfolio.main.exception;
 
 public class CAboutSaveException extends RuntimeException {
+	
     public CAboutSaveException(String msg, Throwable t) {
         super(msg, t);
     }
